80b38077ab4a7ea4edb2064f71ac9360b4b4f251
hungvo2003vn <votanhung2003@gmail.com>
2024-11-03 11:47:41 +0700

    OE-23: add - add authorize

---
+++ online-examination/src/main/java/com/team2/online_examination/annotations/Authorize.java
@@ -0,0 +1,12 @@
+package com.team2.online_examination.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Authorize {
+    String[] roles() default {};
+}
