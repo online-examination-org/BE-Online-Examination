a7cb125f26fd787b1b84a72e26b963cfd1ca95d7
hungvo2003vn <votanhung2003@gmail.com>
2024-11-08 01:14:57 +0700

    hotfix/OE-main: add - transaction configuration

--- online-examination/src/main/java/com/team2/online_examination/configs/TransactionConfig.java
+++ online-examination/src/main/java/com/team2/online_examination/configs/TransactionConfig.java
@@ -1,27 +1,20 @@
 package com.team2.online_examination.configs;
 
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.orm.jpa.JpaTransactionManager;
-import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
 public class TransactionConfig {
 
-    private final DataSource dataSource;
-
-    public TransactionConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
     @Bean
-    public PlatformTransactionManager transactionManager() {
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setDataSource(dataSource);
-
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
         return transactionManager;
     }
 }
