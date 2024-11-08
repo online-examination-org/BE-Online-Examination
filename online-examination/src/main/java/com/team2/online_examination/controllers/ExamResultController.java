95af69cab1f14f4c44701710c6c56cb3d0ca5f7c
Thái Ngọc Rạng <98004370+restom0@users.noreply.github.com>
2024-11-08 12:51:57 +0700

    fix - api student access and create exam resource response

--- online-examination/src/main/java/com/team2/online_examination/controllers/ExamResultController.java
+++ online-examination/src/main/java/com/team2/online_examination/controllers/ExamResultController.java
@@ -1,6 +1,7 @@
 package com.team2.online_examination.controllers;
 
 import com.team2.online_examination.annotations.Authorize;
+import com.team2.online_examination.contexts.StudentContext;
 import com.team2.online_examination.contexts.TeacherContext;
 import com.team2.online_examination.contexts.UserContext;
 import com.team2.online_examination.dtos.requests.ExamResultCreateRequest;
@@ -10,6 +11,7 @@ import com.team2.online_examination.exceptions.NotFoundException;
 import com.team2.online_examination.models.Exam;
 import com.team2.online_examination.models.ExamResult;
 import com.team2.online_examination.services.ExamResultService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -42,14 +44,18 @@ public class ExamResultController {
             return ResponseEntity.internalServerError().body(e.getMessage());
         }
     }
+    @SecurityRequirement(name = "Bearer Authentication")
+    @Authorize(roles = {"student"})
     @PutMapping("/update")
-    public ResponseEntity<?> updateExamResult(@RequestBody ExamResultUpdateRequest examResultUpdateRequest, @RequestParam("id") Long id) {
+    public ResponseEntity<?> updateExamResult(@RequestBody ExamResultUpdateRequest examResultUpdateRequest) {
         // Update exam result
         try{
-            if(id == null){
-                return ResponseEntity.badRequest().body("Id is required");
+            StudentContext studentContext = UserContext.getUserAs(StudentContext.class);
+            System.out.println("Hello");
+            if(studentContext == null){
+                return ResponseEntity.badRequest().body("Student context is required");
             }
-            return ResponseEntity.ok(examResultService.accessExam(examResultUpdateRequest, id));
+            return ResponseEntity.ok(examResultService.accessExam(examResultUpdateRequest, studentContext.getExamResultId()));
         }
         catch (Exception e){
             if(e instanceof NotFoundException){
--- online-examination/src/main/java/com/team2/online_examination/mappers/ExamResultMapper.java
+++ online-examination/src/main/java/com/team2/online_examination/mappers/ExamResultMapper.java
@@ -2,7 +2,6 @@ package com.team2.online_examination.mappers;
 
 import com.team2.online_examination.dtos.requests.ExamResultCreateRequest;
 import com.team2.online_examination.dtos.responses.ExamResultCreateResponse;
-import com.team2.online_examination.dtos.responses.ExamResultUpdateResponse;
 import com.team2.online_examination.models.ExamResult;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
