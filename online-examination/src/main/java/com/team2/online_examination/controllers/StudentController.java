cd2ae0d622d94fa481abd66b56189499ee81ecce
Thái Ngọc Rạng <98004370+restom0@users.noreply.github.com>
2024-11-08 21:31:54 +0700

    OE-79: fix - change id from request to token

--- online-examination/src/main/java/com/team2/online_examination/controllers/StudentController.java
+++ online-examination/src/main/java/com/team2/online_examination/controllers/StudentController.java
@@ -1,9 +1,12 @@
 package com.team2.online_examination.controllers;
 
 import com.team2.online_examination.annotations.Authorize;
+import com.team2.online_examination.contexts.StudentContext;
+import com.team2.online_examination.contexts.UserContext;
 import com.team2.online_examination.dtos.requests.ExamSaveRequest;
 import com.team2.online_examination.dtos.requests.ExamSubmitRequest;
 import com.team2.online_examination.exceptions.AuthenticationFailureException;
+import com.team2.online_examination.exceptions.BadRequestException;
 import com.team2.online_examination.exceptions.GeneralErrorResponse;
 import com.team2.online_examination.models.ExamResult;
 import com.team2.online_examination.services.ExamResultDetailService;
@@ -46,14 +49,18 @@ public class StudentController {
         return this.examService.getExamByPasscode(passcode);
     }
 
-//    @Authorize(roles = {"student"})
+    @Authorize(roles = {"student"})
     @PostMapping("/exam/save")
     public ResponseEntity<?> saveProgress(@RequestBody ExamSaveRequest examSaveRequest) {
         try {
+            StudentContext studentContext = UserContext.getUserAs(StudentContext.class);
+            if(studentContext == null){
+                throw new BadRequestException("Student context is required");
+            }
             Long examResultId = examSaveRequest.getExam_result_id();
             Long questionId = examSaveRequest.getQuestion_id();
             String response = examSaveRequest.getResponse();
-            this.examResultDetailService.updateResponse(examResultId, questionId, response);
+            this.examResultDetailService.updateResponse(studentContext.getExamResultId(), questionId, response);
 
             return ResponseEntity.ok(null);
         } catch (ResponseStatusException e) {
@@ -68,13 +75,17 @@ public class StudentController {
         }
     }
 
-//    @Authorize(roles = {"student"})
+    @Authorize(roles = {"student"})
     @PostMapping("/exam/submit")
     public ResponseEntity<?> submit(@RequestBody ExamSubmitRequest examSubmitRequest) {
         try {
+            StudentContext studentContext = UserContext.getUserAs(StudentContext.class);
+            if(studentContext == null){
+                throw new BadRequestException("Student context is required");
+            }
             Long examResultId = examSubmitRequest.getExam_result_id();
             LocalDateTime finishAt = examSubmitRequest.getFinish_at();
-            this.examResultService.submit(examResultId, finishAt);
+            this.examResultService.submit(studentContext.getExamResultId(), finishAt);
 
             return ResponseEntity.ok(null);
 
