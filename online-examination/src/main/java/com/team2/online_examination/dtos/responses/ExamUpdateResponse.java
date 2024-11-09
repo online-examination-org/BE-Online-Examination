3c81b76ecbdc50420bde99042e6bc2cc759cae9b
Thái Ngọc Rạng <98004370+restom0@users.noreply.github.com>
2024-11-09 08:53:11 +0700

    OE-72: fix - update exam response

--- online-examination/src/main/java/com/team2/online_examination/dtos/responses/ExamUpdateResponse.java
+++ online-examination/src/main/java/com/team2/online_examination/dtos/responses/ExamUpdateResponse.java
@@ -1,18 +1,9 @@
 package com.team2.online_examination.dtos.responses;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.team2.online_examination.models.Teacher;
-import jakarta.persistence.Column;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
-
 import java.time.LocalDateTime;
 
 @Getter
